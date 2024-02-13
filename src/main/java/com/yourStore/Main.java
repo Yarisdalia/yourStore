701060c465df2b8b2a45ae8cde115ece8edf1791
yarita <yarisdaliahinojosapadilla2001@gmail.com>
2024-02-13 15:30:30 -0300

    tipos de variable

--- src/main/java/com/yourStore/Main.java
+++ src/main/java/com/yourStore/Main.java
@@ -1,17 +1,39 @@
 package com.yourStore;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hola, Java!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //variable de tipo cadena de texto
+        String myString = "Esto es una cadena de texto ";
+        myString = "Aqui cambio el valor de la cadena de texto";
+        System.out.println(myString);
+
+        Integer myInt = 7;
+        myInt =  myInt + 4;
+        System.out.println(myInt);
+
+        Double myDouble = 6.5;
+        System.out.println(myDouble);
+
+        Float myFloat = 6.5f;
+        System.out.println(myFloat);
+
+        Boolean myBoolean = true;
+        myBoolean = false;
+        System.out.println(myBoolean);
+
+        myBoolean = null;
+        System.out.println(myBoolean);
+
+        List myList = new ArrayList();
+        myList.add(myString);
+        myList.add(myInt);
+        System.out.println(myString);
     }
+
 }
